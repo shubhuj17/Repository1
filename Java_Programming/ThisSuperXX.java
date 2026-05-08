d28b53e5776ebafd1abb6faa83fe9a17f684115f
shubhuj17 <shubhuj1027@gmail.com>
2026-05-08 23:58:15 +0530

    Add files via upload

---
+++ Java_Programming/ThisSuperXX.java
@@ -0,0 +1,47 @@
+class ThisSuperXX
+{
+    public static void main (String A[])   
+    {
+        Derived dobj = new Derived();
+
+        dobj.gun();
+    }
+}
+
+class Base
+{
+    public int i;
+
+
+    public Base()
+    {
+        System.out.println("Inside Base Constructor");
+        this.i = 11; 
+    }
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived extends Base
+{
+    public int i;
+    
+    public Derived()
+    {
+        System.out.println("Inside Derived Constructor");
+        this.i = 21;
+    }
+
+    public void gun()
+    {
+        System.out.println("Inside gun of Derived");
+        System.out.println("Value of i : "+i);
+        System.out.println("Value of i from bsse : "+super.i);
+       
+    }
+}
+
+
