a147084fac16ec664c82f5d88819ddb2e410f5d2
shubhuj17 <shubhuj1027@gmail.com>
2026-05-12 20:58:12 +0530

    Add files via upload

---
+++ C_Programming/StringPointer.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[] = "Ganesh";
+    char *ptr = NULL;
+
+    ptr = str;              //100
+
+    printf("%c\n",*ptr);    //G
+    ptr++;                  //101
+
+    printf("%c\n",*ptr);    //a
+    ptr++;                  //102
+
+    printf("%c\n",*ptr);    //n
+    ptr++;                  //103
+
+    return 0;
+}
+
+
+
+/*
+output:
+G
+a
+n
+*/
