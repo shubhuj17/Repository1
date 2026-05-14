f5af6c995f6e484f3028f029c1e511681581050f
shubhuj17 <shubhuj1027@gmail.com>
2026-05-15 00:59:21 +0530

    Add files via upload

---
+++ C_Programming/StringLengthUserdefined.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+// Call by address
+int strlenX(char *ptr)
+{
+    int i = 0;
+
+    while( *ptr != '\0')
+    {
+        i++;
+        ptr++;   
+    }
+
+    return i;
+}
+
+int main()
+{
+    char str[] = "Ganesh";      //100
+    int ret = 0;
+
+    ret = strlenX(str);         //strlenX(100)
+
+    printf("String length is: %d\n",ret);
+
+   
+    return 0;
+}
+
+/*
+output:
+String length is: 6
+*/
