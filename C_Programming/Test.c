54500d67974c8b91d0b74c3706d95f79982d3e7f
shubhuj17 <shubhuj1027@gmail.com>
2026-05-18 23:27:15 +0530

    Add files via upload

---
+++ C_Programming/Test.c
@@ -0,0 +1,21 @@
+//demonstration of preproceesor
+/*
+Pre Placement Activity
+22 April 2026
+*/
+
+#include<stdio.h>
+#define PI 3.14
+
+int main()
+{
+    //Area of Circle
+
+    float Area = 0.0f;
+
+    Area = 10.5f*10.5f*PI;
+
+    printf("Area of Circle is : %f\n",Area);
+
+    return 0;
+}
