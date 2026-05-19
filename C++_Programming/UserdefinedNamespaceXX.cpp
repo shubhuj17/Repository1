c787e965ad9aad7185af1086d06a09ec18b54102
shubhuj17 <shubhuj1027@gmail.com>
2026-05-19 13:26:45 +0530

    Add files via upload

---
+++ C++_Programming/UserdefinedNamespaceXX.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+namespace Marvellous
+{
+    int no =11;
+
+    void fun()
+    {
+        std::cout<<"Marvellous\n";
+    }
+}
+
+
+int main()
+{
+    std::cout<<"Jay Ganesh...\n";
+    Marvellous::fun();          
+    return 0;
+}
+
+/*
+output:
+Jay Ganesh...
+Marvellous
+*/
